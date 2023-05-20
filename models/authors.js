ee95261ad70fb911bf3f8f5e541e5e1d6e68973b
Kyle <kylejordanwilliams@gmail.com>
2023-05-20 02:38:41 +0100

    Some of it is working

--- models/authors.js
+++ models/authors.js
@@ -8,9 +8,7 @@ export async function getAuthors() {
 }
 
 export async function searchAuthorsByName(searchTerm) {
-  // Query the database and return all authors that have a name matching the searchTerm
-  const query =
-    "SELECT * FROM authors WHERE first_name LIKE $1 OR last_name LIKE $1";
+  const query = "SELECT * FROM authors WHERE first_name = $1 OR last_name = $1";
   const result = await pool.query(query, [searchTerm]);
   console.log(result.rows);
   return result.rows;
@@ -27,12 +25,8 @@ export async function getAuthorById(id) {
 export async function createAuthor(author) {
   // Query the database to create an author and return the newly created author
   const query =
-    "INSERT INTO authors (id ,first_name, last_name) VALUES ($1, $2) RETURNING *";
-  const result = await pool.query(query, [
-    author.id,
-    author.first_name,
-    author.last_name,
-  ]);
+    "INSERT INTO authors (first_name, last_name) VALUES ($1, $2) RETURNING id";
+  const result = await pool.query(query, [author.first_name, author.last_name]);
   console.log(result.rows);
   return result.rows;
 }
@@ -40,18 +34,20 @@ export async function createAuthor(author) {
 export async function updateAuthorById(id, updates) {
   // Query the database to update an author and return the newly updated author
   const query =
-    "UPDATE authors SET first_name = $1, last_name = $2 WHERE id = $3 RETURNING *";
+    "UPDATE authors SET first_name = $1, last_name = $2 WHERE id = $3";
   const result = await pool.query(query, [
-    id,
     updates.first_name,
     updates.last_name,
+    id,
   ]);
   console.log(result.rows);
   return result.rows;
 }
 
 export async function deleteAuthorById(id) {
-  // Query the database to delete an author and return the deleted author
-
-  return {};
+  // Query the database to delete an author based on the id and return the deleted id
+  const query = "DELETE FROM authors WHERE id = $1";
+  const result = await pool.query(query, [id]);
+  console.log(result.rows);
+  return result.rows;
 }
--- models/books.js
+++ models/books.js
@@ -1,36 +1,57 @@
 import { pool } from "../db/index.js";
 
 export async function getBooks() {
+  const query = "SELECT * FROM books";
   // Query the database and return all books
-  return [];
+  const result = await pool.query(query);
+  return result.rows;
 }
 
 export async function searchBooksByTitle(searchTerm) {
-  // Query the database and return all books that have a matching title matching the searchTerm
-  return [];
+  const query = "SELECT * FROM books WHERE title = $1";
+  const result = await pool.query(query, [searchTerm]);
+  return result.rows;
 }
 
 export async function searchBooksByAuthor(searchTerm) {
-  // Query the database and return all books that have an author name matching the searchTerm
-  return [];
+  const query = `
+    SELECT books.*
+    FROM books
+    INNER JOIN authors ON books.author_id = authors.id
+    WHERE authors.first_name = $1 OR authors.last_name = $1
+  `;
+  const result = await pool.query(query, [searchTerm]);
+  return result.rows;
 }
 
 export async function getBookById(id) {
   // Query the database and return the book with a matching id
-  return {};
+  const query = "SELECT * FROM books WHERE id = $1";
+  const result = await pool.query(query, [id]);
+  return result.rows;
 }
 
 export async function createBook(book) {
   // Query the database to create a book and return the newly created book
-  return {};
+  const query = "INSERT INTO books (title, author_id) VALUES ($1, $2)";
+  const result = await pool.query(query, [book.title, book.author_id]);
+  return result.rows[0];
 }
 
 export async function updateBookById(id, updates) {
   // Query the database to update a book and return the newly updated book
-  return {};
+  const query = "UPDATE books SET title = $1, author_id = $2 WHERE id = $3";
+  const result = await pool.query(query, [
+    updates.title,
+    updates.author_id,
+    id,
+  ]);
+  return result.rows[0];
 }
 
 export async function deleteBookById(id) {
   // Query the database to delete a book and return the deleted book
-  return {};
+  const query = "DELETE FROM books WHERE id = $1";
+  const result = await pool.query(query, [id]);
+  return result.rows[0];
 }
